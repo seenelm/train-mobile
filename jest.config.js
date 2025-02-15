ad0feaae8b605762ed127744bcc95b1311df4027
noahgross1 <noahgross1@gmail.com>
2025-02-15 13:38:18 -0500

    added jest testing

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,17 @@
 module.exports = {
   preset: "react-native",
   testEnvironment: "jest-fixed-jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  testEnvironmentOptions: {
-    customExportConditions: [""],
-  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  // testEnvironmentOptions: {
+  //   customExportConditions: [""],
+  // },
   moduleNameMapper: {
-    "^src/(.*)$": "<rootDir>/src/$1",
+    "^src/(.*)$": "<rootDir>/src/$1", // Match tsconfig paths
   },
+  testMatch: [
+    "<rootDir>/__tests__/**/*.{js,jsx,ts,tsx}", // Find tests in __tests__ folder
+    "<rootDir>/src/**/*.{test,spec}.{js,jsx,ts,tsx}", // Find tests anywhere in src/
+  ],
   globals: {
     Request,
     Response,
